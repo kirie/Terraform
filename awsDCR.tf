3230accc02de9bbf7b9a0e0d5890ea805d33c38c
Eirik <kirie@users.noreply.github.com>
2018-05-24 08:02:34 -0700

    Edit - elastic cidr

--- awsDCR.tf
+++ awsDCR.tf
@@ -4,7 +4,13 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "kirie" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.kirie.id}"
+}
+
+variable "cidrs" { default = [] }
